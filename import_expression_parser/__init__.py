97907cbbb9ce5ade39036c1d001cd98f7c7066da
bmintz <bmintz@protonmail.com>
2018-09-28 15:24:18 +0000

    __init__: let other funcs do the mode validation

--- import_expression_parser/__init__.py
+++ import_expression_parser/__init__.py
@@ -2,22 +2,14 @@
 # encoding: utf-8
 
 import ast as _ast
+import builtins as _builtins
 from importlib import import_module as _import_module
 
 from . import constants as _constants
 from .syntax import fix_syntax as _fix_syntax  # this is for internal use
 from .parser import parse_ast  # we can export this tho
 
-_VALID_MODES = frozenset({'eval', 'exec'})
-_HUMAN_READABLE_MODES = ' or '.join(_VALID_MODES)
-
-eval_globals = {
-	_constants.IMPORTER: _import_module}.copy
-
 def parse(source, *, mode='eval', filename=_constants.DEFAULT_FILENAME):
-	if mode not in _VALID_MODES:
-		raise ValueError(f'mode must be one of {_HUMAN_READABLE_MODES}')
-
-	fixed = _fix_syntax(source, include_import_statement=mode == 'exec')
+	fixed = _fix_syntax(source)
 	tree = _ast.parse(fixed, filename,  mode)
 	return parse_ast(tree, source=fixed)
