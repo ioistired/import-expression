48aabdb70c1ef41d27056aab94c6dc06d76aace2
bmintz <bmintz@protonmail.com>
2018-10-01 15:51:47 +0000

    forgot to export compile

--- import_expression/__init__.py
+++ import_expression/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-from ._public import parse, eval, exec
+from ._public import compile, parse, eval, exec
 from .version import __version__
 
-__all__ = ('parse', 'eval', 'exec')
+__all__ = ('compile', 'parse', 'eval', 'exec')
