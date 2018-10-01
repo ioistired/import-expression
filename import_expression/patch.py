8ceb44ebec401b508917482de6d429c151af1ffd
bmintz <bmintz@protonmail.com>
2018-10-01 07:13:21 +0000

    patch.py: use correct original excepthook

--- import_expression/patch.py
+++ import_expression/patch.py
@@ -17,19 +17,17 @@ def patch(globals=sys.modules['__main__'].__dict__):
 	Both cases will run in the context of the given globals dict, or if None, globals produced by statements will be saved to the __main__ module
 	"""
 	if not _is_tty():
-		raise RuntimeException(f'{__name__} only works at the REPL, where stdin is a TTY.')
+		raise RuntimeException(f'patch() only works at the REPL, where stdin is a TTY.')
 
 	sys.excepthook = _make_excepthook(globals)
 
 def _make_excepthook(globals):
-	original_excepthook = sys.excepthook
-
 	def excepthook(_, error, __):
 		if (
 			type(error) is not SyntaxError
 			or error.lineno != 1  # we don't have all the code
 		):
-			return original_excepthook(type(error), error, error.__traceback__)
+			return sys.__excepthook__(type(error), error, error.__traceback__)
 
 		try:
 			result = import_expression.eval(error.text, globals)
@@ -38,11 +36,11 @@ def _make_excepthook(globals):
 				import_expression.exec(error.text, globals)
 			except SyntaxError:
 				# it must be invalid Python syntax, not invalid import expr syntax
-				return original_excepthook(type(error), error, error.__traceback__)
+				return sys.__excepthook__(type(error), error, error.__traceback__)
 		except SyntaxError:
-			return original_excepthook(type(error), error, error.__traceback__)
+			return sys.__excepthook__(type(error), error, error.__traceback__)
 		except BaseException as error:
-			return original_excepthook(type(error), error, error.__traceback__)
+			return sys.__excepthook__(type(error), error, error.__traceback__)
 		else:
 			if result is None:
 				return
