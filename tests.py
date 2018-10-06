919153ce20405e9c0b9467c86545813b96bcf629
bmintz <bmintz@protonmail.com>
2018-10-06 19:35:48 +0000

    tests: add some more failing tests

--- tests.py
+++ tests.py
@@ -40,6 +40,20 @@ def test_invalid_attribute_syntax():
 		with py.test.raises(SyntaxError):
 			ie.parse(invalid)
 
+def test_invalid_del_store_attribute():
+	for valid in (
+		'a.b!',
+		'a!.b',
+		'a.b.c!.d',
+	):
+		invalid_del = f'del {valid}'
+		invalid_store = f'{valid} = 1'
+
+		for test in invalid_del, invalid_store:
+			print(test)
+			with py.test.raises(SyntaxError):
+				ie.parse(test, mode='exec')
+
 def test_invalid_non_attribute_syntax():
 	for invalid in (
 		'def foo(x!): pass',
@@ -49,6 +63,18 @@ def test_invalid_non_attribute_syntax():
 	):
 		with py.test.raises(SyntaxError):
 			ie.parse(invalid)
+
+def test_del_store_attribute():
+	class AttributeBox:
+		pass
+
+	g = {'x': AttributeBox()}
+
+	ie.exec('x.y = 1', g)
+	assert g['x'].y == 1
+
+	ie.exec('del x.y', g)
+	assert not hasattr(g['x'], 'y')
 
 def test_eval_exec():
 	import collections
@@ -88,16 +114,3 @@ def test_eval_exec():
 	), g)
 
 	assert g['foo'](1) == 'can we make it into jishaku?'
-
-
-def test_load_store_attribute():
-	class AttributeBox:
-		pass
-
-	g = {'x': AttributeBox()}
-
-	ie.exec('x.y = 1', g)
-	assert g['x'].y == 1
-
-	ie.exec('del x.y', g)
-	assert not hasattr(g['x'], 'y')
