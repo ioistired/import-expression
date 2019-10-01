6ef8661e4976c9c897acfdcd4ccb063ac0ff21a1
Io Mintz <io@mintz.cc>
2019-10-01 02:40:18 +0000

    patch: sys.displayhook is a thing

--- import_expression/patch.py
+++ import_expression/patch.py
@@ -36,7 +36,7 @@ def patch(globals=sys.modules['__main__'].__dict__):
 	Both cases will run in the context of the given globals dict, or if None, globals produced by statements will be saved to the __main__ module
 	"""
 	if not _is_tty():
-		raise RuntimeError(f'patch() only works at the REPL, where stdin is a TTY.')
+		raise RuntimeError('patch() only works at the REPL, where stdin is a TTY.')
 
 	sys.excepthook = _make_excepthook(globals)
 
@@ -58,11 +58,7 @@ def _make_excepthook(globals):
 		except BaseException as error:
 			return sys.__excepthook__(type(error), error, error.__traceback__)
 		else:
-			if result is None:
-				return
-
-			builtins._ = result
-			print(repr(result))
+			sys.displayhook(result)
 
 	return excepthook
 
